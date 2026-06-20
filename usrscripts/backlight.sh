9e279664b6dfc24ae5cbfa2b6c7164d90cfa38ec
1hansamal <ihanzamal@gmail.com>
2026-06-20 15:10:32 +0530

    Fix notification replace-id

--- usrscripts/backlight.sh
+++ usrscripts/backlight.sh
@@ -5,7 +5,7 @@ notify ()
 	local brightness
 	brightness=$(brightnessctl -m | awk -F, '{print substr($4, 0, length($4)-1)}')
 
-	notify-send --app-name=brightnessctl --replace-id 99901 -h int:value:"$brightness" "Display Brightness: $brightness %"
+	notify-send --app-name=brightnessctl --replace-id=99901 -h int:value:"$brightness" "Display Brightness: $brightness %"
 }
 
 case $1 in
